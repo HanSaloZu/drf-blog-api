24775e1851b9bf727fd499dd44305e2ed2e5c50e
HanSaloZu <conswintcomin@gmail.com>
2021-02-25 23:06:08 +0300

    Fix ExtendedTestCase

--- utils/test.py
+++ utils/test.py
@@ -6,5 +6,9 @@ class ExtendedTestCase(TestCase):
     UserModel = get_user_model()
 
     def _create_user(self, login, email, password, is_superuser):
-        self.UserModel.objects.create(
-            login=login, email=email, password=password, is_superuser=is_superuser)
+        if is_superuser:
+            return self.UserModel.objects.create_superuser(
+                login=login, email=email, password=password, is_superuser=True)
+
+        return self.UserModel.objects.create_user(
+            login=login, email=email, password=password)
