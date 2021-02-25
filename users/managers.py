9186f67f9d7df99d52048de08a1bf10c09724c2f
HanSaloZu <conswintcomin@gmail.com>
2021-02-25 23:22:42 +0300

    Add is_staff flag processing to UserManager

--- users/managers.py
+++ users/managers.py
@@ -17,12 +17,16 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, login, password, **extra_fileds):
         extra_fileds.setdefault("is_superuser", False)
+        extra_fileds.setdefault("is_staff", False)
         return self._create_user(email, login, password, **extra_fileds)
 
     def create_superuser(self, email, login, password, **extra_fileds):
         extra_fileds.setdefault("is_superuser", True)
+        extra_fileds.setdefault("is_staff", True)
 
         if extra_fileds.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True")
+        if extra_fileds.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_staff=True")
 
         return self._create_user(email, login, password, **extra_fileds)
