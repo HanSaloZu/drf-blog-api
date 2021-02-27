7523441c5b9c53cf0c3791d4577c5e91b6dc07e4
HanSaloZu <conswintcomin@gmail.com>
2021-02-27 23:55:45 +0300

    Add User model to admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "login", "is_staff", "is_superuser")
+    list_display_links = ("id", "email", "login")
+    list_filter = ("is_staff", "is_superuser")
+    search_fields = ("id", "email", "login")
+    fieldsets = (
+        ("Common data", {
+            "fields": ("email", "login", "password")
+        }),
+        ("Advanced options", {
+            "fields": ("is_staff", "is_superuser")
+        })
+    )
