84598cbb79437cc9051875a79f232dbf8b2605ab
HanSaloZu <conswintcomin@gmail.com>
2021-03-03 00:13:38 +0300

    Add user_detail view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .serializers import UserSerializer
+from utils.response import APIResponse
+
+
+@api_view(["GET"])
+def user_detail(request, format=None):  # auth/me
+    user = request.user
+    response = APIResponse()
+
+    if user.is_anonymous:
+        response.result_code = 1
+        response.messages.append("You are not authorized")
+        return response.complete()
+
+    response.data = UserSerializer(user).data
+    return response.complete()
