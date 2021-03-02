5a1a191d381962154ea6098553096e2f4f6d28a6
HanSaloZu <conswintcomin@gmail.com>
2021-03-03 00:32:33 +0300

    Add user authentication view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import user_detail
+from .views import user_detail, user_authentication
 
 urlpatterns = [
-    path("auth/me", user_detail, name="user_detail")
+    path("auth/me", user_detail, name="user_detail"),
+    path("auth/login", user_authentication, name="authentication")
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate, login, logout
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, LoginSerializer
 from utils.response import APIResponse
 
 
@@ -16,3 +17,45 @@ def user_detail(request, format=None):  # auth/me
 
     response.data = UserSerializer(user).data
     return response.complete()
+
+
+@api_view(["POST", "DELETE"])
+def user_authentication(request):
+    response = APIResponse()
+
+    if request.method == "POST":  # login
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            user = authenticate(
+                email=validated_data["email"], password=validated_data["password"])
+
+            if user:
+                login(request, user)
+
+                if not validated_data["rememberMe"]:
+                    request.session.set_expiry(0)
+
+                response.data = {"userId": user.id}
+                return response.complete()
+            else:
+                response.result_code = 1
+                response.messages.append("Incorrect Email or Password")
+                return response.complete()
+
+        fields_errors = serializer.errors
+        for field in fields_errors:
+            message = fields_errors[field][0]
+            response.messages.append(message)
+            response.fields_errors.append({
+                "field": field,
+                "error": message
+            })
+
+        response.result_code = 1
+        return response.complete()
+
+    elif request.method == "DELETE":  # logout
+        logout(request)
+        return response.complete()
