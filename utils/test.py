d809be6adf54560b823fcbf6319bba3f2fe87e89
HanSaloZu <conswintcomin@gmail.com>
2021-03-02 23:05:56 +0300

    Create custom TestCase to testing views

--- utils/test.py
+++ utils/test.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from rest_framework import status
+from django.test import RequestFactory, Client
 from django.contrib.auth import get_user_model
 
 
@@ -12,3 +14,15 @@ class ExtendedTestCase(TestCase):
 
         return self.UserModel.objects.create_user(
             login=login, email=email, password=password)
+
+
+class ViewTestCase(ExtendedTestCase):
+    request_factory = RequestFactory()
+    client = Client()
+
+    def _common_view_tests(self, response, status=status.HTTP_200_OK, result_code=0, messages_list_len=0, fields_errors_list_len=0):
+        self.assertEqual(response.status_code, status)
+        self.assertEqual(response.data["resultCode"], result_code)
+        self.assertEqual(len(response.data["messages"]), messages_list_len)
+        self.assertEqual(
+            len(response.data["fieldsErrors"]), fields_errors_list_len)
