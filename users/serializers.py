e822b38c49efd8783e9d0e3c0765d338de07f984
HanSaloZu <conswintcomin@gmail.com>
2021-03-03 00:31:46 +0300

    Add LoginSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -7,3 +7,29 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "login", "email"]
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(
+        required=True,
+        error_messages={
+            "required": "Please enter your Email",
+            "null": "Please enter your Email",
+            "invalid": "Enter valid Email"
+        })
+    password = serializers.CharField(
+        required=True,
+        error_messages={
+            "required": "Enter your password",
+            "null": "Enter your password"
+        })
+    rememberMe = serializers.BooleanField(
+        required=False,
+        default=False,
+        error_messages={
+            "invalid": "Invalid value for rememberMe"
+        })
+
+    class Meta:
+        model = User
+        fields = ["email", "password", "rememberMe"]
