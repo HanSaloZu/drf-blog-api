7a5a621c3e932b93eba94bfff0f911655cc9a191
HanSaloZu <conswintcomin@gmail.com>
2021-03-03 19:22:43 +0300

    Disable custom exception handler on DEBUG == True

--- utils/handlers.py
+++ utils/handlers.py
@@ -1,10 +1,14 @@
 from rest_framework.views import exception_handler
 from rest_framework import status
 from rest_framework.response import Response
+from django.conf import settings
 
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
+
+    if (settings.DEBUG):
+        raise exc
 
     if not response:
         status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
