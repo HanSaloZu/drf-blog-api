22882bb100da6ee484d8d5843c46b0ddf8d3fef8
HanSaloZu <conswintcomin@gmail.com>
2021-03-03 23:10:03 +0300

    Add profile_detail view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import profile_status_detail
+from .views import profile_status_detail, profile_detail
 
 urlpatterns = [
     path("status/<int:user_id>", profile_status_detail,
-         name="profile_status_detail")
+         name="profile_status_detail"),
+    path("<int:user_id>", profile_detail, name="profile_detail")
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 import json
 
 from .selectors import get_profile_by_user_id
+from .serializers import PhotosExtendedProfileSerializer
 
 
 @api_view(["GET"])
@@ -11,3 +12,9 @@ def profile_status_detail(request, user_id):
     profile = get_profile_by_user_id(user_id)
 
     return HttpResponse(json.dumps(profile.status), content_type="application/json")
+
+
+@api_view(["GET"])
+def profile_detail(request, user_id):
+    profile = get_profile_by_user_id(user_id)
+    return Response(PhotosExtendedProfileSerializer(profile).data)
