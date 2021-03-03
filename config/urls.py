d5aac8d35edb551c1e6881564563d5897f7f0007
HanSaloZu <conswintcomin@gmail.com>
2021-03-03 23:08:54 +0300

    Add static files urls

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -7,3 +9,7 @@ urlpatterns = [
     path("api/1.0/auth/", include("users.urls")),
     path("api/1.0/profile/", include("profiles.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
