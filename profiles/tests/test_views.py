b3a9a683763f02e0a5a0ff7d21f276aee2dd0c36
HanSaloZu <conswintcomin@gmail.com>
2021-03-03 22:55:43 +0300

    Add profile_detail view tests

--- profiles/tests/test_views.py
+++ profiles/tests/test_views.py
@@ -30,3 +30,40 @@ class ProfileStatusDetailViewTest(ViewTestCase):
         self.user.profile.status = "test"
         self.user.save()
         common_status_detail_view_tests(self.client.get(url))
+
+
+class ProfileDetailViewTest(ViewTestCase):
+    def setUp(self):
+        user = self._create_user(login="NewUser", email="new@user.com",
+                                 password="pass", is_superuser=False)
+        profile = user.profile
+
+        profile.looking_for_a_job = True
+        profile.looking_for_a_job_description = "Test"
+        profile.about_me = "I am a view test"
+        profile.contacts.github = "https://github.com/HanSaloZu"
+        user.save()
+        self.profile = profile
+
+    def test_valid_profile_detail(self):
+        url = reverse("profile_detail", kwargs={"user_id": 1})
+        response = self.client.get(url)
+        data = response.data
+        profile = self.profile
+
+        self.assertEqual(data["userId"], profile.user.id)
+        self.assertEqual(data["lookingForAJob"], True)
+        self.assertEqual(data["lookingForAJobDescription"],
+                         profile.looking_for_a_job_description)
+        self.assertEqual(data["fullName"], profile.fullname)
+        self.assertEqual(data["aboutMe"], profile.about_me)
+        self.assertEqual(len(data["contacts"]), 8)
+        self.assertEqual(data["contacts"]["github"], profile.contacts.github)
+        self.assertEqual(len(data["photos"]), 2)
+
+    def test_invalid_profile_detail(self):
+        url = reverse("profile_detail", kwargs={"user_id": 9})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code,
+                         status.HTTP_500_INTERNAL_SERVER_ERROR)
