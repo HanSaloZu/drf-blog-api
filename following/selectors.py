e72cfa47e3e4330aee13367748efb64820c153fb
HanSaloZu <conswintcomin@gmail.com>
2021-03-04 18:44:58 +0300

    Add get_all_user_followings selector

--- following/selectors.py
+++ following/selectors.py
@@ -1,7 +1,13 @@
 from django.contrib.auth import get_user_model
 
+from .models import FollowersModel
+
 User = get_user_model()
 
 
 def get_user_by_id(id):
     return User.objects.get(id=id)
+
+
+def get_all_user_followings(user):
+    return FollowersModel.objects.filter(follower_user=user).only("following_user")
