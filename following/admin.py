ebf77d5d45d729aa94c6c2b144a8d62a3920b4dc
HanSaloZu <conswintcomin@gmail.com>
2021-03-04 15:26:43 +0300

    Add Followers model to admin panel

--- following/admin.py
+++ following/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FollowersModel
+
+
+@admin.register(FollowersModel)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("follower_user", "following_user")
+    list_display_links = ("follower_user", "following_user")
+    list_filter = ("follower_user", "following_user")
+    search_fields = ("follower_user", "following_user")
