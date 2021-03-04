0fff1c2315f4ac904576a1b52f94047a16ecc3e4
HanSaloZu <conswintcomin@gmail.com>
2021-03-04 18:09:43 +0300

    Rename /auth urls file

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/1.0/auth/", include("users.urls")),
+    path("api/1.0/auth/", include("users.auth_urls")),
     path("api/1.0/profile/", include("profiles.urls")),
     path("api/1.0/follow/", include("following.urls"))
 ]
