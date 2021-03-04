de9bf209eea81a004726c524fe504b11b78022c4
HanSaloZu <conswintcomin@gmail.com>
2021-03-04 15:26:10 +0300

    Create Followers model

--- following/models.py
+++ following/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class FollowersModel(models.Model):
+    follower_user = models.ForeignKey(
+        User, related_name="following", on_delete=models.CASCADE)
+    following_user = models.ForeignKey(
+        User, related_name="followers", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ["follower_user", "following_user"]
+        verbose_name = "follower"
+        db_table = "followers"
