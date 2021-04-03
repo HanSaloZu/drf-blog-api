1f015b9c2766bc62ad481472ef54e3082396b0c2
HanSaloZu <conswintcomin@gmail.com>
2021-04-04 00:06:31 +0300

    Remove print

--- middlewares/slash_ignore.py
+++ middlewares/slash_ignore.py
@@ -3,7 +3,6 @@ class SlashIgnoreMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        print(request.path)
         if request.path[-1] == "/":
             request.path = request.path_info = request.path[:-1]
 
