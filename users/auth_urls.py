83861a90c45d1a501f2bf29401d65b236f1f5a9a
HanSaloZu <conswintcomin@gmail.com>
2021-04-03 20:01:21 +0300

    Change login url endpoint

--- users/auth_urls.py
+++ users/auth_urls.py
@@ -4,5 +4,5 @@ from .views import user_detail, user_authentication
 
 urlpatterns = [
     path("me/", user_detail, name="user_detail"),
-    path("login/", user_authentication, name="authentication")
+    path("login", user_authentication, name="authentication")
 ]
