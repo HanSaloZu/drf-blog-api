e04c88c37cf8867758c7223375bfda1092a42c28
HanSaloZu <conswintcomin@gmail.com>
2021-04-03 19:52:15 +0300

    Add get_contacts_by_user_id selector

--- profiles/selectors.py
+++ profiles/selectors.py
@@ -1,5 +1,9 @@
-from .models import Profile
+from .models import Profile, Contacts
 
 
 def get_profile_by_user_id(user_id):
     return Profile.objects.get(user=user_id)
+
+
+def get_contacts_by_user_id(user_id):
+    return Contacts.objects.filter(profile=get_profile_by_user_id(user_id))
