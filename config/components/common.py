a5ced4005b4aa06abd84b9479edbe1f11fcded90
HanSaloZu <conswintcomin@gmail.com>
2021-04-03 22:05:28 +0300

    Split project settings into separate files

---
+++ config/components/common.py
@@ -0,0 +1,79 @@
+from os import environ
+
+
+SECRET_KEY = environ["SECRET_KEY"]
+
+DEBUG = environ["DEBUG"]
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
+
+    "users",
+    "profiles",
+    "following"
+]
+
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware"
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "utils.handlers.custom_exception_handler",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "utils.authentication.CsrfExemptSessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication"
+    )
+}
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+AUTH_USER_MODEL = "users.User"
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Europe/Moscow"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+STATIC_URL = "/static/"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
--- config/settings.py
+++ config/settings.py
@@ -1,136 +1,15 @@
+from split_settings.tools import optional, include
 from os import environ
 from pathlib import Path
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = environ["SECRET_KEY"]
-
-DEBUG = environ["DEBUG"]
-
-ALLOWED_HOSTS = []
-
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'corsheaders',
-
-    'users',
-    'profiles',
-    'following'
+base_settings = [
+    "components/common.py",
+    "components/database.py",
+    "components/cors.py",
+    "components/cookies.py",
 ]
 
-MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware'
-]
-
-REST_FRAMEWORK = {
-    "EXCEPTION_HANDLER": "utils.handlers.custom_exception_handler",
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "utils.authentication.CsrfExemptSessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication"
-    )
-}
-
-ROOT_URLCONF = 'config.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'config.wsgi.application'
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-AUTH_USER_MODEL = 'users.User'
-
-
-CORS_ALLOWED_ORIGINS = []
-
-CORS_ORIGIN_WHITELIST = ()
-
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'set-cookie',
-    'x-requested-with',
-    'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Credentials',
-    'Access-Control-Allow-Origin',
-    'Access-Control-Allow-Methods',
-    'Access-Control-Expose-Headers'
-]
-
-CORS_EXPOSE_HEADERS = [
-    'Content-Type',
-    'X-CSRFToken',
-    'Access-Control-Allow-Origin'
-]
-
-CORS_ALLOW_CREDENTIALS = True
-
-SESSION_COOKIE_AGE = 60 * 60 * 24 * 30  # 30 days
-
-SESSION_COOKIE_SECURE = True
-
-CSRF_COOKIE_SECURE = True
-
-SESSION_COOKIE_HTTPONLY = False
-
-CSRF_COOKIE_HTTPONLY = False
-
-SESSION_COOKIE_SAMESITE = "None"
-
-CRSF_COOKIE_SAMESITE = "None"
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'Europe/Moscow'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-STATIC_URL = '/static/'
-
-MEDIA_URL = "/media/"
-
-MEDIA_ROOT = BASE_DIR / "media"
+include(*base_settings)
