536d7912f3610172233ed7a69cb2053505863b8d
HanSaloZu <conswintcomin@gmail.com>
2021-04-03 23:53:07 +0300

    Remove DRF browsable interface

--- config/components/common.py
+++ config/components/common.py
@@ -52,6 +52,9 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     "EXCEPTION_HANDLER": "utils.handlers.custom_exception_handler",
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "utils.authentication.CsrfExemptSessionAuthentication",
         "rest_framework.authentication.BasicAuthentication"
