886df37ea39c5804b74a8e107905fd6c8198aabc
HanSaloZu <conswintcomin@gmail.com>
2021-04-08 02:05:54 +0300

    Install six

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-from PIL import Image
-
 User = get_user_model()
 
 
@@ -18,6 +16,8 @@ class Profile(models.Model):
         default="", max_length=300, blank=True, null=False)
     about_me = models.TextField(default=None, null=True)
 
+    photo = models.URLField(default=None, max_length=300, null=True)
+
     def __str__(self):
         return f"{self.user.login} profile"
 
@@ -25,36 +25,6 @@ class Profile(models.Model):
         verbose_name = "profile"
         verbose_name_plural = "profiles"
         db_table = "profiles"
-
-
-class Photos(models.Model):
-    profile = models.OneToOneField(
-        Profile, on_delete=models.CASCADE, unique=True)
-    small = models.ImageField(
-        "small photo", default=None, upload_to="photos/small", null=True)
-    large = models.ImageField(
-        "large photo", default=None, upload_to="photos/large", null=True)
-
-    def __str__(self):
-        return f"{self.profile.user.login} photos"
-
-    def save(self, *args, **kwargs):
-        super().save()
-        if (self.small and self.large):
-            photos_data = [
-                {"size": (100, 100), "path": self.small.path},  # small photo
-                {"size": (300, 300), "path": self.large.path}]  # large photo
-
-            for current_photo in photos_data:
-                photo = Image.open(current_photo["path"])
-                resized_photo = photo.resize(current_photo["size"])
-                resized_photo.save(current_photo["path"],
-                                   quality="web_maximum", subsampling=0)
-
-    class Meta:
-        verbose_name = "profile photos"
-        verbose_name_plural = "profiles photos"
-        db_table = "profile_photos"
 
 
 class Contacts(models.Model):
