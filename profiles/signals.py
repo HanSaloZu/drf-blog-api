8b8e353111f2494b3c3ff7be93a8646501603a3b
HanSaloZu <conswintcomin@gmail.com>
2021-04-08 17:45:36 +0300

    Update signals with new model

--- profiles/signals.py
+++ profiles/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
-from .models import Profile, Contacts, Photos
+from .models import Profile, Contacts, Photo
 
 User = get_user_model()
 
@@ -14,12 +14,12 @@ def create_profile(sender, instance, created, **kwargs):
         profile.save()
         contacts = Contacts(profile=profile)
         contacts.save()
-        photos = Photos(profile=profile)
-        photos.save()
+        photo = Photo(profile=profile)
+        photo.save()
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
     instance.profile.contacts.save()
-    instance.profile.photos.save()
+    instance.profile.photo.save()
