ce9813db7b29e647ab9f1d3ec804b38fa13420b4
HanSaloZu <conswintcomin@gmail.com>
2021-04-22 00:47:15 +0300

    Modify custom test cases
    Rename ViewTestCase to APIViewTestCase. Rename _common_view_tests to _common_api_response_tests. Add
    default value to is_superuser flag in _create_user function.

--- utils/test.py
+++ utils/test.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 from rest_framework import status
-from django.test import RequestFactory, Client
+from rest_framework.test import APIRequestFactory, APIClient
 from django.contrib.auth import get_user_model
 
 
 class ExtendedTestCase(TestCase):
     UserModel = get_user_model()
 
-    def _create_user(self, login, email, password, is_superuser):
+    def _create_user(self, login, email, password, is_superuser=False):
         if is_superuser:
             return self.UserModel.objects.create_superuser(
                 login=login, email=email, password=password, is_superuser=True, is_staff=True)
@@ -16,11 +16,11 @@ class ExtendedTestCase(TestCase):
             login=login, email=email, password=password)
 
 
-class ViewTestCase(ExtendedTestCase):
-    request_factory = RequestFactory()
-    client = Client()
+class APIViewTestCase(ExtendedTestCase):
+    request_factory = APIRequestFactory()
+    client = APIClient()
 
-    def _common_view_tests(self, response, status=status.HTTP_200_OK, result_code=0, messages_list_len=0, fields_errors_list_len=0):
+    def _common_api_response_tests(self, response, status=status.HTTP_200_OK, result_code=0, messages_list_len=0, fields_errors_list_len=0):
         self.assertEqual(response.status_code, status)
         self.assertEqual(response.data["resultCode"], result_code)
         self.assertEqual(len(response.data["messages"]), messages_list_len)
