582b3cbc38100bcb8fa7c8d6fcb4d023a925af19
HanSaloZu <conswintcomin@gmail.com>
2021-04-22 01:19:25 +0300

    Update users app views tests
    Split big test functions into small. Rename some variables and classes.

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,106 +1,127 @@
 from django.urls import reverse
 
-from utils.test import ViewTestCase
-from users.views import user_detail
+from utils.test import APIViewTestCase
+from users.views import UserDetail
 
 
-class UserDetailViewTest(ViewTestCase):
-    def setUp(self):
-        self.user = self._create_user(login="NewUser", email="new@user.com",
-                                      password="pass", is_superuser=False)
-        self.url = reverse("user_detail")
+class UserDetailAPIViewTest(APIViewTestCase):
+    url = reverse("user_detail")
 
-    def test_invalid_user_detail(self):
+    def test_unauthorized_user_request_user_detail(self):
         response = self.client.get(self.url)
 
-        self._common_view_tests(response, result_code=1, messages_list_len=1)
+        self._common_api_response_tests(
+            response, result_code=1, messages_list_len=1)
         message = response.data["messages"][0]
         self.assertEqual(message, "You are not authorized")
 
     def test_valid_user_detail(self):
-        user = self.user
+        user = self._create_user(
+            login="NewUser", email="new@user.com", password="pass")
         request = self.request_factory.get(self.url)
+        view = UserDetail()
+
         request.user = user
-        response = user_detail(request)
+        view.setup(request)
+        response = view.get(request)
 
-        self._common_view_tests(response)
-        response_user_data = response.data["data"]
-        self.assertEqual(response_user_data["id"], user.id)
-        self.assertEqual(response_user_data["login"], user.login)
-        self.assertEqual(response_user_data["email"], user.email)
+        self._common_api_response_tests(response)
+        response_user_detail = response.data["data"]
+        self.assertEqual(response_user_detail["id"], user.id)
+        self.assertEqual(response_user_detail["login"], user.login)
+        self.assertEqual(response_user_detail["email"], user.email)
 
 
-class UserAuthenticationViewTest(ViewTestCase):
+class UserAuthenticationAPIViewTest(APIViewTestCase):
+    url = reverse("authentication")
+
     def setUp(self):
-        self.user = self._create_user(login="NewUser", email="new@user.com",
-                                      password="pass", is_superuser=False)
-        self.url = reverse("authentication")
+        self.login = "NewUser"
+        self.email = "new@user.com"
+        self.password = "pass"
+        self.user = self._create_user(login=self.login, email=self.email,
+                                      password=self.password)
 
     def tearDown(self):
-        self.client.delete(self.url)  # logout
+        self.user.delete()
 
-    def test_valid_logging_in(self):
-        def local_common_tests(response):
-            self._common_view_tests(response)
-            response_user_id = response.data["data"]["userId"]
-            self.assertEqual(response_user_id, self.user.id)
-
+    def test_valid_authentication_with_rememberMe_flag(self):
         response = self.client.post(
             self.url, {
-                "email": self.user.email,
-                "password": "pass"
-            })
-        local_common_tests(response)
-
-        response = self.client.post(
-            self.url, {
-                "email": self.user.email,
-                "password": "pass",
+                "email": self.email,
+                "password": self.password,
                 "rememberMe": True
             })
-        local_common_tests(response)
 
-    def test_invalid_logging_in(self):
+        self._common_api_response_tests(response)
+        self.assertEqual(response.data["data"]["userId"], self.user.id)
+
+    def test_valid_authentication_without_rememberMe_flag(self):
+        response = self.client.post(
+            self.url, {
+                "email": self.user.email,
+                "password": self.password
+            })
+
+        self._common_api_response_tests(response)
+        self.assertEqual(response.data["data"]["userId"], self.user.id)
+
+    def test_authentication_with_invalid_email(self):
         response = self.client.post(
             self.url, {
                 "email": "123",
-                "password": "pass"
+                "password": self.password
             })
 
-        self._common_view_tests(response, result_code=1,
-                                messages_list_len=1, fields_errors_list_len=1)
+        self._common_api_response_tests(response, result_code=1,
+                                        messages_list_len=1, fields_errors_list_len=1)
         self.assertEqual(response.data["messages"][0], "Enter valid Email")
         self.assertEqual(response.data["fieldsErrors"][0]["field"], "email")
         self.assertEqual(response.data["fieldsErrors"]
                          [0]["error"], "Enter valid Email")
 
+    def test_authentication_with_invalid_password(self):
         response = self.client.post(
             self.url, {
                 "email": self.user.email,
                 "password": "invalid"
             })
 
-        self._common_view_tests(response, result_code=1,
-                                messages_list_len=1, fields_errors_list_len=0)
+        self._common_api_response_tests(response, result_code=1,
+                                        messages_list_len=1, fields_errors_list_len=0)
         self.assertEqual(response.data["messages"]
                          [0], "Incorrect Email or Password")
 
-        response = self.client.post(self.url)
-        self._common_view_tests(response, result_code=1,
-                                messages_list_len=2, fields_errors_list_len=2)
-
+    def test_authentication_with_invalid_rememberMe_flag(self):
         response = self.client.post(
             self.url, {
                 "email": self.user.email,
-                "password": "invalid",
+                "password": self.password,
                 "rememberMe": "123"
             })
 
-        self._common_view_tests(response, result_code=1,
-                                messages_list_len=1, fields_errors_list_len=1)
+        self._common_api_response_tests(response, result_code=1,
+                                        messages_list_len=1, fields_errors_list_len=1)
         self.assertEqual(response.data["messages"]
                          [0], "Invalid value for rememberMe")
 
+    def test_authentication_wihout_credentials(self):
+        response = self.client.post(self.url)
+
+        self._common_api_response_tests(response, result_code=1,
+                                        messages_list_len=2, fields_errors_list_len=2)
+        self.assertIn("Please enter your Email", response.data["messages"])
+        self.assertIn("Enter your password", response.data["messages"])
+
     def test_logging_out(self):
+        self.client.post(
+            self.url, {
+                "email": self.user.email,
+                "password": self.password
+            })
         response = self.client.delete(self.url)
-        self._common_view_tests(response)
+        self._common_api_response_tests(response)
+
+    def test_unauthorized_logging_out(self):
+        response = self.client.delete(self.url)
+        self._common_api_response_tests(response)
