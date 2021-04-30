6366a905ef0e896db4a15111e4b93219361a190a
HanSaloZu <conswintcomin@gmail.com>
2021-04-30 19:28:41 +0300

    Add follow, unfollow and is_following methods to FollowersModel

--- following/models.py
+++ following/models.py
@@ -10,6 +10,21 @@ class FollowersModel(models.Model):
     following_user = models.ForeignKey(
         User, related_name="followers", on_delete=models.CASCADE)
 
+    @classmethod
+    def follow(self, user, subject):
+        pair = self.objects.create(follower_user=user, following_user=subject)
+        pair.save()
+        return pair
+
+    @classmethod
+    def unfollow(self, user, subject):
+        self.objects.filter(follower_user=user,
+                            following_user=subject).delete()
+
+    @classmethod
+    def is_following(self, user, subject):
+        return self.objects.filter(follower_user=user, following_user=subject).exists()
+
     class Meta:
         unique_together = ["follower_user", "following_user"]
         verbose_name = "follower"
