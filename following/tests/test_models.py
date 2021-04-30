219eb1a60ca2c723de88a2fdd87ca814ea923482
HanSaloZu <conswintcomin@gmail.com>
2021-04-30 19:39:52 +0300

    Modify FollowersModel tests

--- following/tests/test_models.py
+++ following/tests/test_models.py
@@ -1,55 +1,47 @@
 from utils.test import ExtendedTestCase
 from django.db import IntegrityError, transaction
 
-from following.models import FollowersModel
+from ..models import FollowersModel
 
 
 class FollowersModelTest(ExtendedTestCase):
+    model = FollowersModel
+
     def setUp(self):
         self.f_user = self._create_user(
-            login="FirstUser", email="first@user.com", password="pass", is_superuser=False)
+            login="FirstUser", email="first@user.com", password="pass")
         self.s_user = self._create_user(
-            login="SecondUser", email="second@user.com", password="pass", is_superuser=False)
-
-    def _follow(self, follower, follows):
-        return FollowersModel.objects.create(
-            follower_user=follower, following_user=follows)
+            login="SecondUser", email="second@user.com", password="pass")
 
     def tearDown(self):
-        FollowersModel.objects.all().delete()
+        self.model.objects.all().delete()
 
     def test_valid_following(self):
-        pair = self._follow(self.f_user, self.s_user)
-        pair.save()
+        pair = self.model.follow(self.f_user, self.s_user)
 
-        s_user_followers = FollowersModel.objects.filter(
-            following_user=self.s_user)
-        self.assertEqual(s_user_followers.get(follower_user=self.f_user), pair)
+        s_user_followers = self.s_user.followers.all()
+        self.assertEqual(s_user_followers.first(), pair)
 
     def test_unfollow(self):
-        self._follow(self.f_user, self.s_user).save()
-        self._follow(self.s_user, self.f_user).save()
+        self.model.follow(self.f_user, self.s_user)
+        self.model.follow(self.s_user, self.f_user)
 
-        FollowersModel.objects.filter(
-            follower_user=self.f_user).delete()
+        self.model.unfollow(user=self.f_user, subject=self.s_user)
 
-        f_user_follows = FollowersModel.objects.filter(
-            follower_user=self.f_user)
+        f_user_follows = self.f_user.following.all()
         self.assertEqual(f_user_follows.exists(), False)
 
-        s_user_follows = FollowersModel.objects.filter(
-            follower_user=self.s_user)
+        s_user_follows = self.s_user.following.all()
         self.assertEqual(s_user_follows.exists(), True)
         self.assertEqual(s_user_follows[0].following_user, self.f_user)
 
     def test_constraint(self):
-        pair = self._follow(self.f_user, self.s_user)
-        pair.save()
+        self.model.follow(self.f_user, self.s_user)
 
         try:
             # Duplicates should be prevented.
             with transaction.atomic():
-                self._follow(self.f_user, self.s_user)
-            self.fail('Duplicate following allowed.')
+                self.model.follow(self.f_user, self.s_user)
+            self.fail('Duplicate following allowed')
         except IntegrityError:
             pass
