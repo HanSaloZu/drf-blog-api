8ff695643aa856b0c7af931693e816c289f281c3
HanSaloZu <conswintcomin@gmail.com>
2021-05-04 15:31:56 +0300

    Remove useless cors settings

--- config/components/cors.py
+++ config/components/cors.py
@@ -2,28 +2,4 @@ CORS_ALLOWED_ORIGINS = []
 
 CORS_ORIGIN_WHITELIST = ()
 
-CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "set-cookie",
-    "x-requested-with",
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Credentials",
-    "Access-Control-Allow-Origin",
-    "Access-Control-Allow-Methods",
-    "Access-Control-Expose-Headers"
-]
-
-CORS_EXPOSE_HEADERS = [
-    "Content-Type",
-    "X-CSRFToken",
-    "Access-Control-Allow-Origin"
-]
-
 CORS_ALLOW_CREDENTIALS = True
