4f3916a2d6b2fb292b243e16e8880176b528658a
HanSaloZu <conswintcomin@gmail.com>
2021-05-18 21:35:21 +0300

    Update ProfilePreferencesAPIView tests

--- profiles/tests/test_views.py
+++ profiles/tests/test_views.py
@@ -289,9 +289,8 @@ class ProfilePreferencesAPIViewTest(APIViewTestCase):
         response = self.client.put(
             self.url, {"theme": ""}, content_type="application/json")
 
-        self.common_api_response_tests(response)
-        self.assertEqual(
-            self.UserModel.objects.first().profile.preferences.theme, "")
+        self.common_api_response_tests(response, result_code=1, messages_list_len=1,
+                                       fields_errors_list_len=1, messages=["Theme field cannot be empty"])
 
     def test_update_theme_with_null_value(self):
         put_data = {"theme": None}
