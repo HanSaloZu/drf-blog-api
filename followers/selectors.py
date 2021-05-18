95fdf637e609d216376700f3e7b3e398541ee720
HanSaloZu <conswintcomin@gmail.com>
2021-05-19 01:35:46 +0300

    Remove selectors.py from followers app

--- followers/selectors.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-
-def get_user_by_id(user_id):
-    return User.objects.get(id=user_id)
--- followers/views.py
+++ followers/views.py
@@ -2,47 +2,45 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 
-from .selectors import get_user_by_id
 from .models import FollowersModel
 from utils.response import APIResponse
 from utils.views import CustomLoginRequiredMixin
 
+User = get_user_model()
+
 
 class Follow(CustomLoginRequiredMixin, APIView):
-    model = FollowersModel
-
     def get(self, request, user_id):
-        try:
-            subject = get_user_by_id(user_id)
-        except ObjectDoesNotExist:
-            return Response({"message": "Bad request"}, status.HTTP_400_BAD_REQUEST)
+        subject = get_object_or_404(User, id=user_id)
 
-        return Response(self.model.is_following(request.user, subject), status.HTTP_200_OK, content_type="application/json")
+        return Response(FollowersModel.is_following(request.user, subject), status.HTTP_200_OK, content_type="application/json")
 
     def post(self, request, user_id):
         response = APIResponse()
-        subject = get_user_by_id(user_id)
+        subject = get_object_or_404(User, id=user_id)
 
         if user_id == request.user.id:
             response.result_code = 1
             response.messages.append("You can't follow yourself")
             return response.complete()
 
-        if self.model.is_following(request.user, subject):
+        if FollowersModel.is_following(request.user, subject):
             response.result_code = 1
             response.messages.append("You are already following this user")
             return response.complete()
 
-        self.model.follow(request.user, subject)
+        FollowersModel.follow(request.user, subject)
         return response.complete()
 
     def delete(self, request, user_id):
         response = APIResponse()
-        subject = get_user_by_id(user_id)
+        subject = get_object_or_404(User, id=user_id)
 
-        if self.model.is_following(request.user, subject):
-            self.model.unfollow(request.user, subject)
+        if FollowersModel.is_following(request.user, subject):
+            FollowersModel.unfollow(request.user, subject)
 
             return response.complete()
         else:
