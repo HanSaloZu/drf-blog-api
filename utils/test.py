1cef72d2a1c3f89a56c8d52ad15938e45b4d7303
HanSaloZu <conswintcomin@gmail.com>
2021-06-29 19:44:10 +0300

    Remove common_api_response_tests. Add client_error_response_test
    This method makes it easier to test knowingly bad queries

--- utils/test.py
+++ utils/test.py
@@ -12,12 +12,15 @@ class APIViewTestCase(ExtendedTestCase):
     client = APIClient()
     http_status = status
 
-    def common_api_response_tests(self, response, status=http_status.HTTP_200_OK, result_code=0, messages_list_len=0, fields_errors_list_len=0, messages=[]):
+    def client_error_response_test(self, response, code=None,
+                                   status=http_status.HTTP_400_BAD_REQUEST, messages_list_len=0, fields_errors_dict_len=0, messages=[]):
+        if not code is None:
+            self.assertEqual(response.data["code"], code)
+
         self.assertEqual(response.status_code, status)
-        self.assertEqual(response.data["resultCode"], result_code)
         self.assertEqual(len(response.data["messages"]), messages_list_len)
         self.assertEqual(
-            len(response.data["fieldsErrors"]), fields_errors_list_len)
+            len(response.data["fieldsErrors"]), fields_errors_dict_len)
 
         for m in messages:
             self.assertIn(m, response.data["messages"])
