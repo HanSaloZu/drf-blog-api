e3373734c8aa9efa8d9a40d7ff4f1404026b2047
HanSaloZu <conswintcomin@gmail.com>
2021-07-04 13:23:47 +0300

    Add get_profile_by_user_login selector

--- profiles/selectors.py
+++ profiles/selectors.py
@@ -7,3 +7,7 @@ def get_profile_by_user_id(user_id):
 
 def get_contacts_by_user_id(user_id):
     return Contacts.objects.filter(profile=get_profile_by_user_id(user_id))
+
+
+def get_profile_by_user_login(user_login):
+    return Profile.objects.get(user__login=user_login)
