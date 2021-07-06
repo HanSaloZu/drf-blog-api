0e3fd1121b6cc465f41fef0d39747dc487da8bfc
HanSaloZu <conswintcomin@gmail.com>
2021-07-06 18:04:56 +0300

    Add UserFollowersListAPIViewTest

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlencode
 from django.urls import reverse
 
+from followers.models import FollowersModel
 from utils.tests import ProfileDetailAPIViewTestCase, ListAPIViewTestCase
 
 
@@ -158,3 +159,76 @@ class RetrieveUserProfileAPIViewTest(ProfileDetailAPIViewTestCase):
             status=self.http_status.HTTP_404_NOT_FOUND,
             messages_list_len=1,
             messages=["Invalid login, user is not found"])
+
+
+class UserFollowersListAPIViewTest(ListAPIViewTestCase):
+    def url(self, kwargs={}, parameters={}):
+        url = reverse("user_followers_list", kwargs=kwargs)
+        if parameters:
+            url += "?" + urlencode(parameters)
+
+        return url
+
+    def setUp(self):
+        credentials = {"email": "first@gmail.com", "password": "pass"}
+        self.first_user = self.UserModel.objects.create_user(
+            login="FirstUser", **credentials)
+        self.client.login(**credentials)
+
+        self.second_user = self.UserModel.objects.create_user(
+            login="SecondUser", email="second@gmail.com", password="pass")
+        self.third_user = self.UserModel.objects.create_user(
+            login="ThirdUser", email="third@gmail.com", password="pass")
+
+        FollowersModel.follow(self.second_user, self.first_user)
+        FollowersModel.follow(self.third_user, self.first_user)
+
+        FollowersModel.follow(self.first_user, self.second_user)
+        FollowersModel.follow(self.second_user, self.third_user)
+
+    def test_request_by_unauthenticated_client(self):
+        self.client.logout()
+        response = self.client.get(self.url({"login": self.second_user.login}))
+
+        self.unauthorized_client_error_response_test(response)
+
+    def test_followers_list(self):
+        response = self.client.get(self.url({"login": self.second_user.login}))
+
+        self.check_common_response_details(
+            response,
+            total_items=1,
+            page_size=1
+        )
+        self.assertEqual(response.data["items"]
+                         [0]["userId"], self.first_user.id)
+
+        response = self.client.get(self.url({"login": self.third_user.login}))
+
+        self.check_common_response_details(
+            response,
+            total_items=1,
+            page_size=1
+        )
+        self.assertEqual(response.data["items"]
+                         [0]["userId"], self.second_user.id)
+
+    def test_self_followers_list(self):
+        response = self.client.get(self.url({"login": self.first_user.login}))
+
+        self.check_common_response_details(
+            response,
+            total_items=2,
+            page_size=2
+        )
+
+    def test_followers_list_with_invalid_login(self):
+        response = self.client.get(self.url({"login": "Invalid"}))
+
+        self.client_error_response_test(
+            response,
+            code="notFound",
+            status=self.http_status.HTTP_404_NOT_FOUND,
+            messages_list_len=1,
+            messages=["Invalid login, user is not found"]
+        )
