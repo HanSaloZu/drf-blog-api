a39877b792f3b9ff03f33adeba521db92c0d9980
HanSaloZu <conswintcomin@gmail.com>
2021-07-06 18:08:57 +0300

    Add get_profile_by_user_login_or_404 selector

--- profiles/selectors.py
+++ profiles/selectors.py
@@ -1,3 +1,7 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+from utils.exceptions import NotFound404
+
 from .models import Profile, Contacts
 
 
@@ -11,3 +15,10 @@ def get_contacts_by_user_id(user_id):
 
 def get_profile_by_user_login(user_login):
     return Profile.objects.get(user__login=user_login)
+
+
+def get_profile_by_user_login_or_404(user_login):
+    try:
+        return get_profile_by_user_login(user_login)
+    except ObjectDoesNotExist:
+        raise NotFound404("Invalid login, user is not found")
