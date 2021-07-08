bfc218edf5f3442acd454c103950a45fb39d1df0
HanSaloZu <conswintcomin@gmail.com>
2021-07-08 20:42:49 +0300

    Add profile activation services
    Add generate_uidb64 to generate uidb64 and send it to user email in profile activation url.
    Add generate_profile_activation_url to add token and uidb64 to URL parameters.
    Add get_user_by_uidb64_or_none to get the user object using uidb64

---
+++ authentication/services/activation.py
@@ -0,0 +1,33 @@
+from urllib.parse import urlencode, urlparse, parse_qsl, urlunparse
+
+from django.contrib.auth import get_user_model
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.conf import settings
+
+
+User = get_user_model()
+
+
+def generate_uidb64(user):
+    return urlsafe_base64_encode(force_bytes(user.pk))
+
+
+def generate_profile_activation_url(token, uidb64):
+    url_parts = list(urlparse(settings.EMAIL_CONFIRMATION_URL))
+    query = dict(parse_qsl(url_parts[4]))
+    query.update({"token": token, "uidb64": uidb64})
+    url_parts[4] = urlencode(query)
+
+    return urlunparse(url_parts)
+
+
+def get_user_by_uidb64_or_none(uidb64):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    return user
