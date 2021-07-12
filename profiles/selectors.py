6f7e7777647145ffa63f57ccf269f47d689dd4b6
HanSaloZu <conswintcomin@gmail.com>
2021-07-12 21:51:38 +0300

    Remove unused selectors in profiles/selectors.py

--- profiles/selectors.py
+++ profiles/selectors.py
@@ -2,15 +2,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from utils.exceptions import NotFound404
 
-from .models import Profile, Contacts
-
-
-def get_profile_by_user_id(user_id):
-    return Profile.objects.get(user=user_id)
-
-
-def get_contacts_by_user_id(user_id):
-    return Contacts.objects.filter(profile=get_profile_by_user_id(user_id))
+from .models import Profile
 
 
 def get_profile_by_user_login(user_login):
