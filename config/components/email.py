217b1714405c1428d6e6e0c8f46a1e1364ac680d
HanSaloZu <conswintcomin@gmail.com>
2021-07-15 00:00:36 +0300

    Move some email settings to environment variables
    Add EMAIL_HOST, EMAIL_PORT and EMAIL_USE_TLS environment variables

--- config/components/email.py
+++ config/components/email.py
@@ -1,7 +1,7 @@
 EMAIL_CONFIRMATION_URL = environ["EMAIL_CONFIRMATION_URL"]
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = environ["EMAIL_HOST"]
+EMAIL_PORT = int(environ["EMAIL_PORT"])
+EMAIL_USE_TLS = bool(int(environ["EMAIL_USE_TLS"]))
 EMAIL_HOST_USER = environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = environ["EMAIL_HOST_PASSWORD"]
