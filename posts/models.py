82619bb7505f533c61635131a0e0267d7a92cd4e
HanSaloZu <conswintcomin@gmail.com>
2021-07-31 23:53:18 +0300

    Add Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=70, db_index=True)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "post"
+        verbose_name_plural = "posts"
+        db_table = "posts"
+
+    def __str__(self):
+        return self.title
