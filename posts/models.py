caa50f91eb0326170d8dc1ca341bcd985fcda707
HanSaloZu <conswintcomin@gmail.com>
2021-08-01 13:55:19 +0300

    Add Like model

--- posts/models.py
+++ posts/models.py
@@ -18,3 +18,23 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Reaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class Like(Reaction):
+    class Meta:
+        unique_together = ["user", "post"]
+        verbose_name = "like"
+        verbose_name_plural = "likes"
+        db_table = "likes"
+
+    def __str__(self):
+        return f"{self.user.login} liked {self.post.id} post"
+
