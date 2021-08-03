4fd649349f24d26db476ee6fc9facfc496afc813
HanSaloZu <conswintcomin@gmail.com>
2021-08-04 01:39:01 +0300

    Add Post model to admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "author", "title", "created_at")
+    list_display_links = ("id", "author", "title")
+    search_fields = ("id", "author__login", "title")
+    readonly_fields = ("created_at", "updated_at")
+    fieldsets = (
+        (None, {
+            "fields": ("author", "title", "body", "created_at", "updated_at")
+        }),
+    )
+    ordering = ("-created_at",)
