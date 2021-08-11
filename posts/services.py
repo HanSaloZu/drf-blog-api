5f55ea0676b110d776361dd1b71b2806cbce2e35
HanSaloZu <conswintcomin@gmail.com>
2021-08-12 01:13:30 +0300

    Add services.py to posts app
    Services functions are needed to delete and upload posts attachments to Google Drive

---
+++ posts/services.py
@@ -0,0 +1,26 @@
+from profiles.services.google_drive_api import GoogleDriveAPI
+from profiles.services.images import update_image
+
+from .models import Attachment
+
+
+google_drive = GoogleDriveAPI()
+
+
+def delete_post_attachments(post):
+    attachments = Attachment.objects.all().filter(post=post)
+
+    for attachment in attachments:
+        google_drive.delete_file(attachment.file_id)
+        attachment.delete()
+
+
+def create_post_attachment(post, file):
+    attachment = Attachment.objects.create(
+        post=post, file_id="", link="")
+    update_image(attachment, file)
+
+
+def delete_post(post):
+    delete_post_attachments(post)
+    post.delete()
