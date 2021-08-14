cce988a41f564798ec8bfeade48271c92e0e70e3
HanSaloZu <conswintcomin@gmail.com>
2021-08-15 00:15:47 +0300

    Change /followers/ endpoint url to /profile/followers/

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/auth/", include("authentication.urls")),
     path("api/v1/profile/", include("profiles.urls")),
-    path("api/v1/followers/", include("followers.followers_urls")),
+    path("api/v1/profile/followers/", include("followers.followers_urls")),
     path("api/v1/profile/following/", include("followers.following_urls")),
     path("api/v1/users/", include("users.urls")),
     path("api/v1/posts/", include("posts.urls")),
