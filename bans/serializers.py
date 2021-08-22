c70f7e6a271f9a5a7b3bf77f65594766ea6c595c
HanSaloZu <conswintcomin@gmail.com>
2021-08-22 18:52:48 +0300

    Add BanSerializer

--- bans/serializers.py
+++ bans/serializers.py
@@ -5,6 +5,24 @@ from users.serializers import UserSerializer
 from .models import Ban
 
 
+class BanSerializer(serializers.ModelSerializer):
+    reason = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        allow_null=False,
+        max_length=250,
+        error_messages={
+            "null": "Reason cannot be null",
+            "invalid": "Invalid value for reason",
+            "max_length": "Reason must be up to 250 characters long"
+        }
+    )
+
+    class Meta:
+        model = Ban
+        fields = ["reason"]
+
+
 class BannedUserSerializer(serializers.ModelSerializer):
     receiver = UserSerializer()
     creator = UserSerializer()
