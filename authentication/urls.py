0bf2620ace0bb3d6790aecc5d8f858d65190b22b
HanSaloZu <conswintcomin@gmail.com>
2021-09-03 17:28:25 +0300

    Remove AuthenticationAPIView

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
 
-from .views import AuthenticationAPIView
 
-urlpatterns = [
-    path("", AuthenticationAPIView.as_view(), name="authentication")
-]
+urlpatterns = []
