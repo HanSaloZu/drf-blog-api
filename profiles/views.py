6d7effd26196117c55e393b7b69623cb09123d36
HanSaloZu <conswintcomin@gmail.com>
2021-09-03 14:52:17 +0300

    Remove useless import

--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import update_session_auth_hash
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
-from rest_framework.generics import ListAPIView
 
 from utils.views import LoginRequiredAPIView
 from utils.shortcuts import raise_400_based_on_serializer
