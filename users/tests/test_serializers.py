b44ff6e7bc145805d97d7dfda3d845bf14cd6fcc
HanSaloZu <conswintcomin@gmail.com>
2021-09-04 19:16:45 +0300

    Add CreateUserSerializerTestCase

--- users/tests/test_serializers.py
+++ users/tests/test_serializers.py
@@ -1,6 +1,7 @@
 from utils.tests import ExtendedTestCase
+from utils.shortcuts import generate_messages_list_by_serializer_errors
 
-from ..serializers import UserSerializer
+from ..serializers import UserSerializer, CreateUserSerializer
 
 
 class UserSerializerTestCase(ExtendedTestCase):
@@ -24,3 +25,63 @@ class UserSerializerTestCase(ExtendedTestCase):
 
         self.assertEqual(len(serializer.data), 4)
         self.assertIs(serializer.data["isAdmin"], True)
+
+
+class CreateUserSerializerTestCase(ExtendedTestCase):
+    serializer_class = CreateUserSerializer
+
+    def setUp(self):
+        self.user = self.UserModel.objects.create_user(
+            login="User", email="unique@email.com", password="pass")
+
+    def test_valid_serializer(self):
+        data = {
+            "login": "NewUser",
+            "email": "new@user.com",
+            "password1": "strongpassword",
+            "password2": "strongpassword"
+        }
+        serializer = self.serializer_class(data=data)
+
+        self.assertIs(serializer.is_valid(), True)
+        validated_data = dict(serializer.validated_data)
+        self.assertEqual(data, validated_data)
+
+    def test_invalid_serializer(self):
+        data = {
+            "login": "New:;.!?@User",
+            "email": None,
+            "password1": "s"
+        }
+        serializer = self.serializer_class(data=data)
+
+        self.assertIs(serializer.is_valid(), False)
+
+        errors = generate_messages_list_by_serializer_errors(serializer.errors)
+        self.assertEqual(len(errors), 4)
+        self.assertIn(
+            ("Login can only contain English letters, numbers, " +
+                "underscores and hyphens"),
+            errors
+        )
+        self.assertIn("Email is required", errors)
+        self.assertIn("Password must be at least 4 characters", errors)
+        self.assertIn("You should repeat your password", errors)
+
+    def test_serializer_with_non_unique_login(self):
+        """
+        Using a non-unique login makes the serializer invalid
+        """
+        data = {
+            "login": "User",
+            "email": "new@user.com",
+            "password1": "strongpassword",
+            "password2": "strongpassword"
+        }
+        serializer = self.serializer_class(data=data)
+
+        self.assertIs(serializer.is_valid(), False)
+
+        errors = generate_messages_list_by_serializer_errors(serializer.errors)
+        self.assertEqual(len(errors), 1)
+        self.assertIn("This login is already in use", errors)
