5aef237189ac81a023c46965973cc8ab096e3c56
HanSaloZu <conswintcomin@gmail.com>
2021-09-05 22:22:12 +0300

    Modify send_profile_activation_email
    Rename it to send_verification_email. Update context, subject and template path

--- verification/services/email.py
+++ verification/services/email.py
@@ -1,9 +1,6 @@
 from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 
-from ..tokens import confirmation_token
-from .activation import generate_uidb64, generate_profile_activation_url
-
 
 def send_email(email_subject, email_template, email_context, to_email):
     message = render_to_string(email_template, email_context)
@@ -13,15 +10,11 @@ def send_email(email_subject, email_template, email_context, to_email):
     email.send()
 
 
-def send_profile_activation_email(user):
-    token = confirmation_token.make_token(user)
-    uidb64 = generate_uidb64(user)
-    url = generate_profile_activation_url(token, uidb64)
-
+def send_verification_email(user, verification_code):
     email_context = {
         "user": user,
-        "url": url
+        "code": verification_code
     }
 
-    send_email("Activate your profile", "email/activate_profile.html",
+    send_email("Verify your email", "email/verify_email.html",
                email_context, user.email)
