019f8b1f3d6452ad00d9dc83a25dc3c5ad63ecd0
HanSaloZu <conswintcomin@gmail.com>
2021-09-07 15:19:48 +0300

    Add VerificationCode model to admin panel

--- verification/admin.py
+++ verification/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from utils.admin import (NoAddPermissionAdminModel,
+                         NoChangePermissionAdminModel,
+                         NoDeletePermissionAdminModel)
+
+from .models import VerificationCode
+
+
+@admin.register(VerificationCode)
+class VerificationCodeAdmin(NoAddPermissionAdminModel,
+                            NoChangePermissionAdminModel,
+                            NoDeletePermissionAdminModel):
+    list_display = ("user", "code", "created_at")
+    list_display_links = ("user", "code")
+    search_fields = ("user__login", "code")
+    ordering = ("-created_at",)
+    fieldsets = (
+        (None, {
+            "fields": ("user", "code", "created_at")
+        }),
+    )
