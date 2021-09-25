bf818ddc4413107bd76d1be6d43d971511b37f16
HanSaloZu <conswintcomin@gmail.com>
2021-09-25 23:53:54 +0300

    Add CustomTokenRefreshAPIViewTestCase

--- authentication/tests/test_views.py
+++ authentication/tests/test_views.py
@@ -1,12 +1,11 @@
 from django.urls import reverse
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from utils.tests import APIViewTestCase
 from bans.services import ban
 
 
-class CustomObtainTokenPairAPIViewTestCase(APIViewTestCase):
-    url = reverse("token_create")
-
+class BaseTestCase:
     def setUp(self):
         self.credentials = {"email": "new@user.com", "password": "pass"}
         self.inactive_user_credentials = {
@@ -27,6 +26,10 @@ class CustomObtainTokenPairAPIViewTestCase(APIViewTestCase):
         admin = self.UserModel.objects.create_superuser(
             login="Admin", email="admin@user.com", password="pass")
         ban(receiver=self.banned_user, creator=admin)
+
+
+class CustomObtainTokenPairAPIViewTestCase(BaseTestCase, APIViewTestCase):
+    url = reverse("token_create")
 
     def test_valid_token_obtaining(self):
         """
@@ -92,4 +95,81 @@ class CustomObtainTokenPairAPIViewTestCase(APIViewTestCase):
             response,
             fields_errors_dict_len=2,
             messages=["Enter your email", "Enter your password"]
+        )
+
+
+class CustomTokenRefreshAPIViewTestCase(BaseTestCase, APIViewTestCase):
+    url = reverse("token_refresh")
+
+    def test_valid_token_refreshing(self):
+        """
+        Valid token refreshing should return a 200 status code and a token pair
+        """
+        payload = {
+            "refresh": str(RefreshToken.for_user(self.user))
+        }
+        response = self.client.post(self.url, payload)
+
+        self.assertEqual(response.status_code, self.http_status.HTTP_200_OK)
+        self.assertIn("refresh", response.data)
+        self.assertIn("access", response.data)
+
+    def test_invalid_token_refreshing(self):
+        """
+        Invalid token refreshing should return a 401 error
+        """
+        payload = {
+            "refresh": "invalid"
+        }
+        response = self.client.post(self.url, payload)
+
+        self.client_error_response_test(
+            response,
+            code="notAuthenticated",
+            status=self.http_status.HTTP_401_UNAUTHORIZED,
+            messages=["You are not authenticated"]
+        )
+
+    def test_token_refreshing_without_payload(self):
+        """
+        Token refreshing without payload should return a 400 error
+        """
+        response = self.client.post(self.url, {})
+
+        self.client_error_response_test(
+            response,
+            messages=["Refresh token is required"],
+            fields_errors_dict_len=1
+        )
+
+    def test_token_refreshing_by_banned_user(self):
+        """
+        Token refreshing by banned users should return a 403 error
+        """
+        payload = {
+            "refresh": str(RefreshToken.for_user(self.banned_user))
+        }
+        response = self.client.post(self.url, payload)
+
+        self.client_error_response_test(
+            response,
+            code="bannedUser",
+            status=self.http_status.HTTP_403_FORBIDDEN,
+            messages=["You are banned"]
+        )
+
+    def test_token_refreshing_by_inactive_user(self):
+        """
+        Token refreshing by inactive users should return a 401 error
+        """
+        payload = {
+            "refresh": str(RefreshToken.for_user(self.inactive_user))
+        }
+        response = self.client.post(self.url, payload)
+
+        self.client_error_response_test(
+            response,
+            code="notAuthenticated",
+            status=self.http_status.HTTP_401_UNAUTHORIZED,
+            messages=["You are not authenticated"]
         )
