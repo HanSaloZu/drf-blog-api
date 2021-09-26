5aac9a24c276317f9ad21f729c1bb63a6450f842
HanSaloZu <conswintcomin@gmail.com>
2021-09-27 00:06:35 +0300

    Modify ListCreateUsersAPIView. Add registration
    Prevent creating users by admins

--- users/views.py
+++ users/views.py
@@ -1,45 +1,45 @@
-from rest_framework import serializers
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.generics import RetrieveAPIView
-from django.urls import reverse
 
-from authentication.serializers import RegistrationSerializer
 from utils.views import LoginRequiredAPIView
-from utils.exceptions import Forbidden403
+from utils.exceptions import Forbidden403, NotAuthenticated401
 from utils.shortcuts import raise_400_based_on_serializer
 from profiles.serializers import ProfileSerializer
 from profiles.selectors import get_profile_by_user_login_or_404
 from followers.selectors import (get_user_followers_ids_list,
                                  get_user_followings_ids_list)
 from posts.mixins import ListPostsWithOrderingAPIViewMixin
+from verification.services.codes import create_verification_code
+from verification.services.email import send_verification_email
 
 from .mixins import ListUsersAPIViewMixin
+from .serializers import CreateUserSerializer
 
 
-class ListCreateUsersAPIView(LoginRequiredAPIView, ListUsersAPIViewMixin):
+class ListCreateUsersAPIView(ListUsersAPIViewMixin):
     """
     Lists all users or creates one
     """
 
-    def post(self, request):
-        if not request.user.is_staff:
-            raise Forbidden403(
-                "You don't have permission to access this resource")
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            raise NotAuthenticated401
+        return super().get(request, *args, **kwargs)
 
-        serializer = RegistrationSerializer(data=request.data)
+    def post(self, request):
+        if request.user.is_authenticated:
+            raise Forbidden403("You are already authenticated")
+
+        serializer = CreateUserSerializer(data=request.data)
 
         if serializer.is_valid():
             user = serializer.save()
-            user.is_active = True
-            user.save()
 
-            return Response(
-                data=ProfileSerializer(user.profile).data,
-                status=HTTP_201_CREATED,
-                headers={"Location": reverse(
-                    "user_profile_detail", kwargs={"login": user.login})}
-            )
+            verification_code = create_verification_code(user).code
+            send_verification_email(user, verification_code)
+
+            return Response(status=HTTP_204_NO_CONTENT)
 
         raise_400_based_on_serializer(serializer)
 
