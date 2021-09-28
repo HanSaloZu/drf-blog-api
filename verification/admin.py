e21785a8587364eaa92baf253d16e86b0190cad7
HanSaloZu <conswintcomin@gmail.com>
2021-09-29 01:52:45 +0300

    Allow removal of verification codes from admin panel

--- verification/admin.py
+++ verification/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 
-from utils.admin import (NoAddPermissionAdminModel,
-                         NoChangePermissionAdminModel,
-                         NoDeletePermissionAdminModel)
+from utils.admin import NoAddPermissionAdminModel, NoChangePermissionAdminModel
 
 from .models import VerificationCode
 
 
 @admin.register(VerificationCode)
 class VerificationCodeAdmin(NoAddPermissionAdminModel,
-                            NoChangePermissionAdminModel,
-                            NoDeletePermissionAdminModel):
+                            NoChangePermissionAdminModel):
     list_display = ("user", "code", "created_at")
     list_display_links = ("user", "code")
     search_fields = ("user__login", "code")
