db7ff1a3ea04d8b13a212a26da7e90840de98d5d
HanSaloZu <conswintcomin@gmail.com>
2021-09-30 19:52:57 +0300

    Reduce JWT refresh token lifetime to 7 days

--- config/components/jwt.py
+++ config/components/jwt.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
     "ALGORITHM": "HS256",
