8230fcb5f31353fd030a1a0974bbf24354339d43
HanSaloZu <conswintcomin@gmail.com>
2021-10-01 13:44:36 +0300

    Add SessionAuthentication to DEFAULT_AUTHENTICATION_CLASSES

--- config/components/rest.py
+++ config/components/rest.py
@@ -1,10 +1,10 @@
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework.renderers.JSONRenderer",
-        "rest_framework.renderers.BrowsableAPIRenderer"
     ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication"
     ),
     "EXCEPTION_HANDLER": "utils.exceptions.custom_exception_handler",
     "TEST_REQUEST_DEFAULT_FORMAT": "json"
