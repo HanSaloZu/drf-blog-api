87097a468c1af021482480d2bfabb05b58e605a1
HanSaloZu <conswintcomin@gmail.com>
2021-11-25 00:25:26 +0300

    Fix UpdateImageMixin error when uploaded image is None

--- profiles/mixins.py
+++ profiles/mixins.py
@@ -9,10 +9,15 @@ from .services.images import update_instance_image
 class UpdateImageMixin:
     image_field = None
 
+    def is_file_an_image(self, image):
+        if isinstance(image, File):
+            return image.content_type.split("/")[0] == "image"
+        return False
+
     def put(self, request):
         image = request.data.get(self.image_field)
 
-        if image.content_type.split("/")[0] == "image":
+        if self.is_file_an_image(image):
             instance = self.get_object(request)
             link_to_image = update_instance_image(instance, image)
 
